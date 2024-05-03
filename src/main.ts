93ae6e88cccdc73c6ef93891d9f3b7560ec4cf2d
Leon Radley <leon.radley@locka.com>
2024-05-03 10:58:38 +0200

    bug 🪳

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { parseISO } from 'date-fns/parseISO';
+import { format } from 'date-fns/format';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
@@ -6,7 +8,8 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+  const date = parseISO('2024-01-01');
+  res.send({ message: `Hello ${format(date, 'yyyy-MM-dd')}` });
 });
 
 app.listen(port, host, () => {
